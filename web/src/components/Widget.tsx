91328a58e3587c0326752a95395ee755bd576d90
Mateus Abelli <43862225+MateusAbelli@users.noreply.github.com>
2022-05-08 20:58:10 -0300

    Added Widget component

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,15 @@
+import { ChatTeardropDots } from "phosphor-react";
+
+export function Widget() {
+  return (
+    <div className="absolute right-5 bottom-5">
+      <button className="bg-brand-500 rounded-full p-3 text-white flex group">
+        <ChatTeardropDots className="h-6 w-6" />
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="mx-1">Feedback</span>
+        </span>
+      </button>
+    </div>
+  );
+}
+
