b6250fcb5df4a772846a87f67f2b42d09a9f077d
Mateus Abelli <43862225+MateusAbelli@users.noreply.github.com>
2022-05-08 22:16:46 -0300

    Updated widget to use the new panel

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,13 +1,16 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
+import { WidgetPanel } from "./WidgetPanel";
 
 export function Widget() {
   return (
-    <Popover className="absolute right-5 bottom-5">
-      <Popover.Panel>Panel Placeholder</Popover.Panel>
+    <Popover className="absolute right-5 bottom-5 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetPanel />
+      </Popover.Panel>
       <Popover.Button className="bg-brand-500 rounded-full p-3 text-white flex group">
         <ChatTeardropDots className="h-6 w-6" />
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs group-focus-visible:max-w-xs transition-all duration-500 ease-linear">
           <span className="mx-1">Feedback</span>
         </span>
       </Popover.Button>
